cfa9f67babeb40d4555557fef963457d9f4e0e4f
Zhao Tang <zxt@zhaotang.ca>
2021-03-09 18:12:55 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# SimplePlatformer
+
+A simple platformer game in javascript
+
---
+++ src/js/Coin.js
@@ -0,0 +1,42 @@
+import State from "./State.js";
+import Vec from "./Vec.js";
+
+class Coin {
+    constructor(pos, basePos, wobble) {
+        this.pos = pos;
+        this.basePos = basePos;
+        this.wobble = wobble;
+    }
+
+    get type() {
+        return "coin";
+    }
+
+    static create(pos) {
+        let basePos = pos.plus(new Vec(0.2, 0.1));
+        return new Coin(basePos, basePos, Math.random() * Math.PI * 2);
+    }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+Coin.prototype.collide = function(state) {
+    let filtered = state.actors.filter(a => a != this);
+    let status = state.status;
+    if (!filtered.some(a => a.type == "coin")) {
+        status = "won";
+    }
+    return new State(state.level, filtered, status);
+}
+
+const wobbleSpeed = 8;
+const wobbleDist = 0.07;
+
+Coin.prototype.update = function(time) {
+    let wobble = this.wobble + time * wobbleSpeed;
+    let wobblePos = Math.sin(wobble) * wobbleDist;
+    return new Coin(this.basePos.plus(new Vec(0, wobblePos)),
+        this.basePos, wobble);
+}
+
+export default Coin;
